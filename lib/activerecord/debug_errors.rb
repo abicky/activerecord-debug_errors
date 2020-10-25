53a232da57af968c187673d166f40d2701a2c954
abicky <takeshi.arabiki@gmail.com>
2020-10-26 02:34:23 +0900

    Initialize gem by "bundle gem --no-coc activerecord-debug_errors"

---
+++ lib/activerecord/debug_errors.rb
@@ -0,0 +1,8 @@
+require "activerecord/debug_errors/version"
+
+module Activerecord
+  module DebugErrors
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/activerecord/debug_errors/version.rb
@@ -0,0 +1,5 @@
+module Activerecord
+  module DebugErrors
+    VERSION = "0.1.0"
+  end
+end
